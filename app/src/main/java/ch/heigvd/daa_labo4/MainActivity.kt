227b6a9b340633293c7ad45fda68c47062d8f378
Nicolas Crausaz <n.crausaz99@gmail.com>
2022-12-09 08:41:18 +0100

    Base project

---
+++ app/src/main/java/ch/heigvd/daa_labo4/MainActivity.kt
@@ -0,0 +1,11 @@
+package ch.heigvd.daa_labo4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
